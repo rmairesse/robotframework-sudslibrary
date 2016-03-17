4224b22cb6592dd4cf67816aa43f4ffac8d0cd47
Andreas Hindborg <andreas.hindborg@moveinnovation.dk>
2016-03-17 13:28:42 +0100

    Fixes for python3 support.

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import sys
 from os.path import join, dirname
 
-sys.path.append(join(dirname(__file__), 'src'))
-from ez_setup import use_setuptools
-use_setuptools()
+#sys.path.append(join(dirname(__file__), 'src'))
+#from ez_setup import use_setuptools
+#use_setuptools()
 from setuptools import setup
 
-execfile(join(dirname(__file__), 'src', 'SudsLibrary', 'version.py'))
+exec(compile(open(join(dirname(__file__), 'src', 'SudsLibrary', 'version.py')).read(), join(dirname(__file__), 'src', 'SudsLibrary', 'version.py'), 'exec'))
 
 DESCRIPTION = """
 SudsLibrary is a web service testing library for Robot Framework
@@ -26,13 +26,7 @@ Topic :: Software Development :: Testing
 
 # determine whether to use Jurko's fork of Suds. This will only work for source
 # distributions.
-jurko = False
-try:
-    import suds
-    jurko = 'jurko' in suds.__version__
-except:
-    pass
-suds_rqmnt = 'suds >= 0.4' if not jurko else 'suds-jurko'
+suds_rqmnt = 'suds-jurko'
 suds_rqmnt = os.environ.get('SUDS_LIBRARY_SUDS_REQUIREMENT', suds_rqmnt)
 
 
@@ -53,5 +47,6 @@ setup(name         = 'robotframework-sudslibrary',
                             'robotframework >= 2.6.0',
                          ],
       package_dir  = {'' : 'src'},
-      packages     = ['SudsLibrary']
+      packages     = ['SudsLibrary'],
+      use_2to3 = True
       )
